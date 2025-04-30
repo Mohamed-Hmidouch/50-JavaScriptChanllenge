83c294eb4ef69ace86adb2aad15e5ffea0d7817e
Mohamed-Hmidouch <ffa54202@gmail.com>
2025-04-30 19:09:46 +0200

    Easy-Basic-Data-Manipulation

---
+++ Easy-Basic.js
@@ -0,0 +1,59 @@
+import employees from './employees.js';
+
+
+// function getFirstEmployeeFirstName(){
+//  for (let i = 0; i < employees.length; i++) {
+//     const element = employees[0].firstName;
+//     return element;
+//  }
+// }
+
+// const firstEmployeeFirstName = getFirstEmployeeFirstName(employees);
+// console.log(firstEmployeeFirstName);
+/////////////////////////////////////////////////////////////////////////////
+// function getLastEmployeeLastName(){
+//      for (let i = 0; i < employees.length; i++) {
+//     const element = employees[employees.length-1].lastName;
+//     return element;
+//      }
+// }
+
+// const lastEmployeeLastName = getLastEmployeeLastName(employees);
+// console.log(lastEmployeeLastName);
+
+// function getEmployeeDepartmentById(employees,id){
+//    for (let i = 0; i < employees.length; i++) {
+//     const department = employees[i].id;
+//     if(department == id){
+//         return employees[i].department;
+//     }
+//    }
+// }
+
+// const employeeDepartmentById = getEmployeeDepartmentById(employees,5);
+// console.log(employeeDepartmentById);
+
+
+// function countEmployees(employees){
+//     let count = 0;
+//    for (let i = 0; i < employees.length; i++) {
+//     count++;
+    
+//    }
+//    return count;
+// }
+
+// const countEmployes = countEmployees(employees);
+// console.log(countEmployes);
+
+// function hasInactiveEmployees(){
+//   for (let i = 0; i < employees.length; i++) {
+//     const isActive = employees[i].isActive;
+//     if(isActive == false){
+//      return true;
+//     }
+//   }
+// }
+
+// const inactiveEmployees = hasInactiveEmployees(employees);
+// console.log(inactiveEmployees);
